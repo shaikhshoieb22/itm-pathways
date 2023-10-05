5e35defd534767e8cf91a262bfcf5f353d2deae4
Shoieb Shaikh <shoiebs@letsupgrade.in>
2023-10-05 18:00:32 +0530

    first commit

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/newpage.html',
+                '/styles.css',
+                '/logo.png',
+                '/favicon.ico'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
